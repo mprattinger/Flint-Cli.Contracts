0b48cf06856dc9a4af4f869fb8f8bf0fc8a6a027
Michael Prattinger <mprattinger@outlook.com>
2017-11-28 13:08:55 +0100

    Change in PluginInfo Class

--- Flint-Cli.Contracts/PluginInfo.cs
+++ Flint-Cli.Contracts/PluginInfo.cs
@@ -3,7 +3,6 @@
     public class PluginInfo
     { 
         public string UniquePluginName { get; set; }
-        public string ShortName { get; set; }
-        public string LongName { get; set; }
+        public string PluginCommand { get; set; }
     }
 }
